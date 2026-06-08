785ee7713244e7a9e7d6b952c6c2ba3e12dc4382
glhalton <glhalton.amaro@gmail.com>
2026-06-08 12:30:34 -0300

    Primeiro commit

---
+++ src/modules/books/books.service.ts
@@ -0,0 +1,19 @@
+export const getBookService = async () => {
+    
+}
+
+export const listBooksService = async () => {
+    
+}
+
+export const createBookService = async () => {
+
+}
+
+export const updateBookService = async () => {
+    
+}
+
+export const deleteBookService = async () => {
+    
+}
